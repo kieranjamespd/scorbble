36f7cb64dd3519eb8770e2680207b404d238aa87
kieranjamespd <kieran.james93@gmail.com>
2025-12-07 20:41:35 +0000

    Initial Commit

---
+++ Scorbble/ScorbbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScorbbleApp.swift
+//  Scorbble
+//
+//  Created by Kieran James on 07.12.25.
+//
+
+import SwiftUI
+
+@main
+struct ScorbbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScorbbleTests/ScorbbleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScorbbleTests.swift
+//  ScorbbleTests
+//
+//  Created by Kieran James on 07.12.25.
+//
+
+import Testing
+@testable import Scorbble
+
+struct ScorbbleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
